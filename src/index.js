12dd690e8bbbd8346417113b9d538fb39c67271c
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2021-09-26 22:26:57 +0700

    feat: add section video

--- src/index.js
+++ src/index.js
@@ -3,3 +3,17 @@ import './assets/fonts/fonts.css';
 import './css/styles.scss';
 
 // connect with js files
+//temporarily here
+
+const PROGRESS = document.querySelector('.progress-bar');
+const VOLUME = document.querySelector('.volume-bar');
+
+PROGRESS.addEventListener('input', function(e) {
+  const value = e.target.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+})
+
+VOLUME.addEventListener('input', function(e) {
+  const value = e.target.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
+})
