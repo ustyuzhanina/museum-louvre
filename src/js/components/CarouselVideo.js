65325737cedb06a34c7087f9d40d4abfde508869
ustyuzhanina <elena.ustyuzhanina.rus@gmail.com>
2022-02-17 19:08:08 +0700

    feat: start adding YT player functions for iframes slider into code

--- src/js/components/CarouselVideo.js
+++ src/js/components/CarouselVideo.js
@@ -3,16 +3,16 @@ import 'slick-carousel';
 
 export default class CarouselVideo {
   constructor() {
-    this.container = document.querySelector('.subcontainer__video-slider');
-    this.poster = document.querySelector('.video-poster');
-    this.posterStyle = window.getComputedStyle(this.poster);
-
+    // this.container = document.querySelector('.subcontainer__video-slider');
+    // this.poster = document.querySelector('.video-poster');
+    // this.posterStyle = window.getComputedStyle(this.poster);
+    this.sliderClass = '.subcontainer__video-slider';
     this.render = this.render.bind(this);
     this.setEventListeners = this.setEventListeners.bind(this);
   }
 
   render() {
-    $('.subcontainer__video-slider').slick({
+    $(this.sliderClass).slick({
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 1,
@@ -32,5 +32,35 @@ export default class CarouselVideo {
     });
   }
 
-  setEventListeners() {}
+  setEventListeners() {
+    const iframesArray = document.querySelectorAll(`.video-poster__iframe`);
+
+    // This code loads the IFrame Player API code asynchronously.
+    const tag = document.createElement('script');
+    tag.src = 'http://www.youtube.com/player_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // This function creates an <iframe> (and YouTube player)
+    //    after the API code downloads.
+
+    iframesArray.forEach((iframe, i) => {
+      const uri = iframe.src;
+      const videoId = uri.match(/embed\/?/)[0];
+      let player;
+      function onYouTubePlayerAPIReady() {
+        player = new YT.Player(`video-poster__iframe_${iframe[i]}`, {
+          height: '254',
+          width: '452',
+          videoId: videoId,
+          events: {
+            onReady: onPlayerReady,
+            onStateChange: onPlayerStateChange,
+          },
+        });
+      }
+    });
+
+    $(this.sliderClass).on('beforeChange', function (event, slick, currentSlide, nextSlide) {});
+  }
 }
